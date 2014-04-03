2f244111eab2becba86fd0b6a92412c8d1fc67e6
Oliver Delevingne <oliver@delevingne.co.uk>
2014-04-03 16:18:29 +0100

    removed put commands and replaced with print commands, using \n at the end of each string

--- directory.rb
+++ directory.rb
@@ -1,21 +1,21 @@
 def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "-------------"
+  print "The students of my cohort at Makers Academy\n"
+  print "-------------\n"
 end
 
-def print(students)
+def input(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    print "#{student[:name]} (#{student[:cohort]} cohort)\n"
   end
 end
 
 def print_footer(names)    
-  puts "Overall, we have #{names.length} great students"
+  print "Overall, we have #{names.length} great students\n"
 end
 
 def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  print "Please enter the names of the students\n"
+  print "To finish, just hit return twice\n"
   # create an empty array
   students = []
   # get the first name
@@ -24,7 +24,7 @@ def input_students
   while !name.empty? do    
     # add the student hash to the array
     students << {:name => name, :cohort => :november}    
-    puts "Now we have #{students.length} students"
+    print "Now we have #{students.length} students\n"
     # get another name from the user
     name = gets.chomp
   end
@@ -34,5 +34,5 @@ end
 
 students = input_students
 print_header
-print(students)
+input(students)
 print_footer(students)
