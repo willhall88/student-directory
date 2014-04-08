f2a00fccf5b847c119222166d81b82d2085183d4
Oliver Delevingne <oliver@delevingne.co.uk>
2014-04-08 14:17:59 +0100

    updated code, still at index number stage

--- directory.rb
+++ directory.rb
@@ -1,38 +1,41 @@
-def print_header
-  print "The students of my cohort at Makers Academy\n"
-  print "-------------\n"
-end
-
-def print_out(students)
-  students.each_with_index do |student, index|
-    print "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)\n"
-  end
-end
-
-def print_footer(names)    
-  print "Overall, we have #{names.length} great students\n"
-end
-
 def input_students
-  print "Please enter the names of the students\n"
-  print "To finish, just hit return twice\n"
+  puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do    
-    # add the student hash to the array
-    students << {:name => name, :cohort => :november}    
-    print "Now we have #{students.length} students\n"
+  # while the name is not empty, repear this code
+  while !name.empty? do
+    # add teh student hash to the array
+    students << {:name => name, :cohort => :march}
+    puts "Now we have #{students.length} students"
     # get another name from the user
     name = gets.chomp
-  end
-  # return the array of students
+  end 
+  # return the array of student
   students
 end
 
-students = input_students
+
+def print_header
+  puts "The students of my cohort at Makers Academy"
+  puts "---------"
+end
+
+def print_students(students)
+  students.each_with_index do |student, index|
+    puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+
+def print_footer(names)
+puts "Overall, we have #{names.length} great students"
+end 
+
+# nothing happens until we call the methods
+students = input_students # this sets the variable students as the method 'input_students'. This variable then gets used in the following methods.
 print_header
-print_out(students)
+print_students(students)
 print_footer(students)
